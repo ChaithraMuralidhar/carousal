34845319b312532c7d397dafeba8ab56d60acae6
ChaithraMuralidhar <chaithramuralidhar.official@gmail.com>
2023-03-14 11:27:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';//referencing the package name here
+import "react-responsive-carousel/lib/styles/carousel.min.css";//importing the css file 
+
+let styles = {
+  margin: 'auto',
+  width: '500px'
+};
+
+function App() {
+return (
+<div style={styles}>
+  <Carousel>
+ {/* here we are pulling images using <carousal></carousal> */}
+    <div>
+      <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
+      <p className="legend">Hong Kong</p>
+    </div>
+    <div>
+      <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore"/>
+      <p className="legend">London</p>
+    </div>
+    <div>
+      <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" alt="Japan"/>
+      <p className="legend">Japan</p>
+    </div>
+    <div>
+      <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
+      <p className="legend">Las Vegas</p>
+    </div>
+  </Carousel>
+</div>
+);
+}
+
+export default App;
